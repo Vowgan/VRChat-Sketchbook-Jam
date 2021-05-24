bc617395284c11e091d5dfd77c5b193d936c507b
Vowgan <47050502+Vowgan@users.noreply.github.com>
2021-05-23 20:56:13 -0500

    Setup Pixel Palette

---
+++ Assets/_Vowgan/Scripts/InkSpotDetection.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class InkSpotDetection : UdonSharpBehaviour
+{
+    
+    public Camera cam;
+    public Color startColor = new Color(0.1f, 0.1f, 0.1f);
+    
+    private void Start()
+    {
+        cam.enabled = false;
+        GetComponent<MeshRenderer>().material.color = startColor;
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name.Contains("Palette"))
+        {
+            cam.enabled = true;
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name.Contains("Palette"))
+        {
+            cam.enabled = false;
+        }
+    }
+}
