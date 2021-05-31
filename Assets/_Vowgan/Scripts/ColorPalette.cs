e6bf0134b5d1ccf40b30d4120f53e9657aa5fb20
Hakkniv <hakkniv@gmail.com>
2021-05-31 01:17:12 -0500

    Added color modification functionality
    + Added Color Sliders
    + Added a save color function that saves to an additional, initially blank palette
    + Added a 'Display Current Pen Color' function that takes the current color of the pen and displays it with the sliders allowing it to be saved or modified.

--- Assets/_Vowgan/Scripts/ColorPalette.cs
+++ Assets/_Vowgan/Scripts/ColorPalette.cs
@@ -2,43 +2,111 @@
 using System;
 using UdonSharp;
 using UnityEngine;
+using UnityEngine.UI;
 using VRC.SDKBase;
 using VRC.Udon;
 
 public class ColorPalette : UdonSharpBehaviour
 {
     
-    // TODO: Color palette selection/saving
-    
-    public Color[] paletteColors = new Color[42];
+    public SpriteRenderer[] savedColors;
+    public int currentSaveIndex = 0;
+    public Image colorPreview;
 
-    public float redVal;
-    public float greenVal;
-    public float blueVal;
+    private float oldRedVal;
+    private float oldGreenVal;
+    private float oldBlueVal;
+    
+    [UdonSynced]public float redVal;
+    [UdonSynced]public float greenVal;
+    [UdonSynced]public float blueVal;
+
+    public Slider redSlider;
+    public Slider greenSlider;
+    public Slider blueSlider;
+    
+    public ModifyPenProxy penProxy;
     
     void Start()
     {
         
     }
 
-    public void setColor(int selection)
+    public void setOwner()
     {
-        paletteColors[selection] = new Color(redVal, greenVal, blueVal);
+        Networking.SetOwner(Networking.LocalPlayer, gameObject);
     }
 
-    public void setRed(float val)
+    public override void OnDeserialization()
     {
-        redVal = val;
+        if (redVal != oldRedVal)
+        {
+            oldRedVal = redVal;
+            redSlider.value = redVal;
+        }
+        
+        if (blueVal != oldBlueVal)
+        {
+            oldBlueVal = blueVal;
+            blueSlider.value = blueVal;
+        }
+        
+        if (greenVal != oldGreenVal)
+        {
+            oldGreenVal = greenVal;
+            greenSlider.value = greenVal;
+        }
+    }
+
+    public void setColor()
+    {
+        colorPreview.color = new Color(redVal, greenVal, blueVal);
+    }
+
+    public void setRed()
+    {
+        redVal = redSlider.value;
+        RequestSerialization();
+        setColor();
     }
     
-    public void setBlue(float val)
+    public void setGreen()
     {
-       blueVal = val;
+        greenVal = greenSlider.value;
+        RequestSerialization();
+        setColor();
     }
     
-    public void setGreen(float val)
+    public void setBlue()
     {
-       greenVal = val;
+        blueVal = blueSlider.value;
+        RequestSerialization();
+        setColor();
+    }
+
+    public void saveColor()
+    {
+        savedColors[currentSaveIndex].enabled = true;
+        savedColors[currentSaveIndex].color = new Color(redVal, greenVal, blueVal);
+        
+        currentSaveIndex += 1;
+        if (currentSaveIndex >= 42)
+        {
+            currentSaveIndex = 0;
+        }
+    }
+    
+    public void modifyColor()
+    {
+        Color penColor = penProxy.onePixel.GetPixel(0, 0);
+        
+        Debug.Log( $"Red + {penColor.r * 255}" );
+        Debug.Log( $"Green + {penColor.g * 255}" );
+        Debug.Log( $"Blue + {penColor.b * 255}" );
+        
+        redSlider.value = penColor.r;
+        greenSlider.value = penColor.g;
+        blueSlider.value = penColor.b;
     }
     
 }
---
+++ Assets/_Vowgan/Scripts/ModifyPenProxy.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ModifyPenProxy : UdonSharpBehaviour
+{
+    public Texture2D onePixel;
+    public Camera stylusCamera;
+    
+    private Rect _cameraRect;
+    
+    void Start()
+    {
+        _cameraRect = stylusCamera.pixelRect;
+    }
+
+    private void OnPostRender()
+    {
+        onePixel.ReadPixels(_cameraRect, 0, 0, false);
+        onePixel.Apply( false );
+    }
+}
